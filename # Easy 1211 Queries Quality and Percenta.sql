229507d12c19fcdb301d8fd93269713050c09087
anyuleannli <48267768+anyuleannli@users.noreply.github.com>
2020-06-16 18:47:53 -0700

    Add files via upload

---
+++ # Easy 1211 Queries Quality and Percenta.sql
@@ -0,0 +1,5 @@
+# Easy 1211 Queries Quality and Percentage
+
+select query_name, round(avg(rating/position),2) as quality, round(sum(case when rating <3 then 1 else 0 end)*100/count(*),2) as poor_query_percentage
+from Queries
+group by query_name
